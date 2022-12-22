1221c88287993751a4aa69b6f87bf7ea9d7ff8ed
Kewin Titus <kewintitus@gmail.com>
2022-12-22 19:37:15 +0530

    update

--- start.js
+++ start.js
@@ -20,9 +20,10 @@
 // Run `npm install` to install them.
 // 'path' is part of Node.js and thus not inside package.json.
 //-------------------------------------------------------------------
-var express = require('express');           // For web server
-var Axios = require('axios');               // A Promised base http client
-var bodyParser = require('body-parser');    // Receive JSON format
+var express = require('express'); // For web server
+var Axios = require('axios'); // A Promised base http client
+var bodyParser = require('body-parser'); // Receive JSON format
+require('dotenv').config();
 
 // Set up Express web server
 var app = express();
@@ -32,7 +33,7 @@ app.use(express.static(__dirname + '/www'));
 // This is for web server to start listening to port 3000
 app.set('port', 3000);
 var server = app.listen(app.get('port'), function () {
-    console.log('Server listening on port ' + server.address().port);
+  console.log('Server listening on port ' + server.address().port);
 });
 
 //-------------------------------------------------------------------
@@ -48,58 +49,61 @@ const querystring = require('querystring');
 
 // // Route /api/forge/oauth
 app.get('/api/forge/oauth', function (req, res) {
-    Axios({
-        method: 'POST',
-        url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-        },
-        data: querystring.stringify({
-            client_id: FORGE_CLIENT_ID,
-            client_secret: FORGE_CLIENT_SECRET,
-            grant_type: 'client_credentials',
-            scope: scopes
-        })
+  Axios({
+    method: 'POST',
+    url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+    data: querystring.stringify({
+      client_id: FORGE_CLIENT_ID,
+      client_secret: FORGE_CLIENT_SECRET,
+      grant_type: 'client_credentials',
+      scope: scopes,
+    }),
+  })
+    .then(function (response) {
+      // Success
+      access_token = response.data.access_token;
+      console.log(response);
+      res.redirect('/api/forge/datamanagement/bucket/create');
     })
-        .then(function (response) {
-            // Success
-            access_token = response.data.access_token;
-            console.log(response);
-            res.redirect('/api/forge/datamanagement/bucket/create');
-        })
-        .catch(function (error) {
-            // Failed
-            console.log(error);
-            res.send('Failed to authenticate');
-        });
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send('Failed to authenticate');
+    });
 });
 
 // Route /api/forge/oauth/public
 app.get('/api/forge/oauth/public', function (req, res) {
-    // Limit public token to Viewer read only
-    Axios({
-        method: 'POST',
-        url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-        },
-        data: querystring.stringify({
-            client_id: FORGE_CLIENT_ID,
-            client_secret: FORGE_CLIENT_SECRET,
-            grant_type: 'client_credentials',
-            scope: 'viewables:read'
-        })
+  // Limit public token to Viewer read only
+  Axios({
+    method: 'POST',
+    url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+    },
+    data: querystring.stringify({
+      client_id: FORGE_CLIENT_ID,
+      client_secret: FORGE_CLIENT_SECRET,
+      grant_type: 'client_credentials',
+      scope: 'viewables:read',
+    }),
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      res.json({
+        access_token: response.data.access_token,
+        expires_in: response.data.expires_in,
+      });
     })
-        .then(function (response) {
-            // Success
-            console.log(response);
-            res.json({ access_token: response.data.access_token, expires_in: response.data.expires_in });
-        })
-        .catch(function (error) {
-            // Failed
-            console.log(error);
-            res.status(500).json(error);
-        });
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.status(500).json(error);
+    });
 });
 
 // Buckey key and Policy Key for OSS
@@ -108,131 +112,142 @@ const policyKey = 'transient'; // Expires in 24hr
 
 // Route /api/forge/datamanagement/bucket/create
 app.get('/api/forge/datamanagement/bucket/create', function (req, res) {
-    // Create an application shared bucket using access token from previous route
-    // We will use this bucket for storing all files in this tutorial
-    Axios({
-        method: 'POST',
-        url: 'https://developer.api.autodesk.com/oss/v2/buckets',
-        headers: {
-            'content-type': 'application/json',
-            Authorization: 'Bearer ' + access_token
-        },
-        data: JSON.stringify({
-            'bucketKey': bucketKey,
-            'policyKey': policyKey
-        })
+  // Create an application shared bucket using access token from previous route
+  // We will use this bucket for storing all files in this tutorial
+  Axios({
+    method: 'POST',
+    url: 'https://developer.api.autodesk.com/oss/v2/buckets',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: 'Bearer ' + access_token,
+    },
+    data: JSON.stringify({
+      bucketKey: bucketKey,
+      policyKey: policyKey,
+    }),
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      res.redirect('/api/forge/datamanagement/bucket/detail');
     })
-        .then(function (response) {
-            // Success
-            console.log(response);
-            res.redirect('/api/forge/datamanagement/bucket/detail');
-        })
-        .catch(function (error) {
-            if (error.response && error.response.status == 409) {
-                console.log('Bucket already exists, skip creation.');
-                res.redirect('/api/forge/datamanagement/bucket/detail');
-                return;
-            }
-            // Failed
-            console.log(error);
-            res.send('Failed to create a new bucket');
-        });
+    .catch(function (error) {
+      if (error.response && error.response.status == 409) {
+        console.log('Bucket already exists, skip creation.');
+        res.redirect('/api/forge/datamanagement/bucket/detail');
+        return;
+      }
+      // Failed
+      console.log(error);
+      res.send('Failed to create a new bucket');
+    });
 });
 
 // Route /api/forge/datamanagement/bucket/detail
 app.get('/api/forge/datamanagement/bucket/detail', function (req, res) {
-    Axios({
-        method: 'GET',
-        url: 'https://developer.api.autodesk.com/oss/v2/buckets/' + encodeURIComponent(bucketKey) + '/details',
-        headers: {
-            Authorization: 'Bearer ' + access_token
-        }
+  Axios({
+    method: 'GET',
+    url:
+      'https://developer.api.autodesk.com/oss/v2/buckets/' +
+      encodeURIComponent(bucketKey) +
+      '/details',
+    headers: {
+      Authorization: 'Bearer ' + access_token,
+    },
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      res.redirect('/upload.html');
     })
-        .then(function (response) {
-            // Success
-            console.log(response);
-            res.redirect('/upload.html');
-        })
-        .catch(function (error) {
-            // Failed
-            console.log(error);
-            res.send('Failed to verify the new bucket');
-        });
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send('Failed to verify the new bucket');
+    });
 });
 
 // For converting the source into a Base64-Encoded string
 var Buffer = require('buffer').Buffer;
 String.prototype.toBase64 = function () {
-    // Buffer is part of Node.js to enable interaction with octet streams in TCP streams, 
-    // file system operations, and other contexts.
-    return new Buffer(this).toString('base64');
+  // Buffer is part of Node.js to enable interaction with octet streams in TCP streams,
+  // file system operations, and other contexts.
+  return new Buffer(this).toString('base64');
 };
 
-var multer = require('multer');         // To handle file upload
+var multer = require('multer'); // To handle file upload
 var upload = multer({ dest: 'tmp/' }); // Save file into local /tmp folder
 
 // Route /api/forge/datamanagement/bucket/upload
-app.post('/api/forge/datamanagement/bucket/upload', upload.single('fileToUpload'), function (req, res) {
+app.post(
+  '/api/forge/datamanagement/bucket/upload',
+  upload.single('fileToUpload'),
+  function (req, res) {
     var fs = require('fs'); // Node.js File system for reading files
     fs.readFile(req.file.path, function (err, filecontent) {
-        Axios({
-            method: 'PUT',
-            url: 'https://developer.api.autodesk.com/oss/v2/buckets/' + encodeURIComponent(bucketKey) + '/objects/' + encodeURIComponent(req.file.originalname),
-            headers: {
-                Authorization: 'Bearer ' + access_token,
-                'Content-Disposition': req.file.originalname,
-                'Content-Length': filecontent.length
-            },
-            data: filecontent
+      Axios({
+        method: 'PUT',
+        url:
+          'https://developer.api.autodesk.com/oss/v2/buckets/' +
+          encodeURIComponent(bucketKey) +
+          '/objects/' +
+          encodeURIComponent(req.file.originalname),
+        headers: {
+          Authorization: 'Bearer ' + access_token,
+          'Content-Disposition': req.file.originalname,
+          'Content-Length': filecontent.length,
+        },
+        data: filecontent,
+      })
+        .then(function (response) {
+          // Success
+          console.log(response);
+          var urn = response.data.objectId.toBase64();
+          res.redirect('/api/forge/modelderivative/' + urn);
         })
-            .then(function (response) {
-                // Success
-                console.log(response);
-                var urn = response.data.objectId.toBase64();
-                res.redirect('/api/forge/modelderivative/' + urn);
-            })
-            .catch(function (error) {
-                // Failed
-                console.log(error);
-                res.send('Failed to create a new object in the bucket');
-            });
+        .catch(function (error) {
+          // Failed
+          console.log(error);
+          res.send('Failed to create a new object in the bucket');
+        });
     });
-});
+  }
+);
 
 // Route /api/forge/modelderivative
 app.get('/api/forge/modelderivative/:urn', function (req, res) {
-    var urn = req.params.urn;
-    var format_type = 'svf';
-    var format_views = ['2d', '3d'];
-    Axios({
-        method: 'POST',
-        url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/job',
-        headers: {
-            'content-type': 'application/json',
-            Authorization: 'Bearer ' + access_token
-        },
-        data: JSON.stringify({
-            'input': {
-                'urn': urn
-            },
-            'output': {
-                'formats': [
-                    {
-                        'type': format_type,
-                        'views': format_views
-                    }
-                ]
-            }
-        })
+  var urn = req.params.urn;
+  var format_type = 'svf';
+  var format_views = ['2d', '3d'];
+  Axios({
+    method: 'POST',
+    url: 'https://developer.api.autodesk.com/modelderivative/v2/designdata/job',
+    headers: {
+      'content-type': 'application/json',
+      Authorization: 'Bearer ' + access_token,
+    },
+    data: JSON.stringify({
+      input: {
+        urn: urn,
+      },
+      output: {
+        formats: [
+          {
+            type: format_type,
+            views: format_views,
+          },
+        ],
+      },
+    }),
+  })
+    .then(function (response) {
+      // Success
+      console.log(response);
+      res.redirect('/viewer.html?urn=' + urn);
     })
-        .then(function (response) {
-            // Success
-            console.log(response);
-            res.redirect('/viewer.html?urn=' + urn);
-        })
-        .catch(function (error) {
-            // Failed
-            console.log(error);
-            res.send('Error at Model Derivative job.');
-        });
+    .catch(function (error) {
+      // Failed
+      console.log(error);
+      res.send('Error at Model Derivative job.');
+    });
 });
